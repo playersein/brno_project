c0464cfde925458e1a48d84e3debe41661d558aa
명세인 <seinmyeong@myeongsein-ui-MacBookAir.local>
2025-01-21 21:55:40 +0900

    Create func: save the output

--- brno_check.py
+++ brno_check.py
@@ -20,3 +20,25 @@ def check_business_status(api_key, business_number):
             return f"Error {response.status_code}"
     except Exception as e:
         return f"Error{str(e)}"
+    
+def process_business_numbers(csv_file, api_key, ouput_file):
+    """
+    CSV 파일에서 사업자번호 읽기
+    """
+    # CSV 파일에서 사업자번호 읽기
+    data = pd.read_csv(csv_file)
+    if "business_number" not in data.columns:
+        raise ValueError("CSV 파일에 business_number 열이 필요합니다.")
+    
+    results = []
+    for business_number in data["busniess_number"]:
+        status = check_business_status(api_key, str(business_number))
+        results.append({"business_number": business_number, "python": status})
+        
+    # 결과를 dataframe으로 변환
+    results_df = pd.DataFrame(results)
+    
+    # 결과를 Excel 파일로 저장
+    results_df.to_excel(ouput_file, index=False)
+    print(f"결과가 {ouput_file}에 저장되었습니다.")
+    
